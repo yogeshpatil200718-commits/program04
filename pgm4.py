a0c28dcc8722cc31427078b35d8bb820a1a5eeb7
Yogesh Patil <yogeshpatil200718@gmail.com>
2026-09-15 12:08:05 +0530

    Multiplication and division

--- pgm4.py
+++ pgm4.py
@@ -3,3 +3,9 @@ b = float(input("Enter second number: "))
 
 print("Addition =", a + b)
 print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+
+if b != 0:
+    print("Division =", a / b)
+else:
+    print("Division = Cannot divide by zero")
